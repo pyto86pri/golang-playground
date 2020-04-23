0f8f22417c285b1f1c0d239f58c21822dd4b2d45
pyto86 <pyto86.me@gmail.com>
2020-04-23 12:04:18 +0900

    section 2 completed

--- A_Tour_of_Go/main.go
+++ A_Tour_of_Go/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"runtime"
 )
 
 // UpperCamelCase
@@ -18,6 +20,40 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func nsum(n int) (sum int) {
+	if n < 0 {
+		return
+	}
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+	return
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func myOs() {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s", os)
+	}
+}
+
+func deferHello() {
+	defer fmt.Println("World!")
+
+	fmt.Println("Hello")
+}
+
 var c, python, java bool = true, true, false
 
 func main() {
@@ -26,5 +62,9 @@ func main() {
 	var i, j int = 1, 2
 	k := 3
 	fmt.Println(i, j, k, c, python, java)
-
+	fmt.Println(nsum(10))
+	fmt.Println(nsum(-2))
+	fmt.Println(pow(2, 10, 2000))
+	myOs()
+	deferHello()
 }
